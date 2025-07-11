84a633549b84322b6efd77f807a3a70bc9086247
BestVex <bestvex.oly@gmail.com>
2025-07-10 19:25:11 -0700

    Chassis Done

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {1, -2, -3},     // Left Chassis Ports (negative port will reverse it!)
-    {-4, 6, 7},  // Right Chassis Ports (negative port will reverse it!)
+    {-7, 8, -5},     // Left Chassis Ports (negative port will reverse it!)
+    {9, -4, 1},  // Right Chassis Ports (negative port will reverse it!)
 
     21,      // IMU Port
     3.25,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
